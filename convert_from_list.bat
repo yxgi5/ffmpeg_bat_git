e5a6726fb640b611e2de73f1901f6b25aea4bbfb
Andreas Zhang <denglitsch@gmail.com>
2024-04-12 11:09:36 +0800

    update:

--- convert_from_list.bat
+++ convert_from_list.bat
@@ -1,4 +1,14 @@
 @echo off
 setlocal EnableDelayedExpansion
 chcp 65001
-for /f "delims=" %%i in (list.txt) do ffmpeg_bat_git.bat "%%i"
+
+SET "SRC_FILE="
+
+if [%1] neq [] (
+    SET SRC_FILE=%1
+) else (
+    SET SRC_FILE=list.txt
+)
+echo SRC_FILE=%SRC_FILE%
+
+for /f "delims=" %%i in (%SRC_FILE%) do ffmpeg_bat_git.bat "%%i"
