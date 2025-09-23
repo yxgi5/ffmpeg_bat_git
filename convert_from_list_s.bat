4f0cfdca6ad2ce182e2775d49b6a12a34d585084
Andreas Zhang <yxgi5@163.com>
2025-09-23 17:31:53 +0800

    update:

---
+++ convert_from_list_s.bat
@@ -0,0 +1,15 @@
+@echo off
+setlocal EnableDelayedExpansion
+chcp 65001
+
+SET "SRC_FILE="
+
+if [%1] neq [] (
+    SET SRC_FILE=%1
+) else (
+    SET SRC_FILE=list.txt
+)
+echo SRC_FILE=%SRC_FILE%
+
+::for /f "delims=" %%i in (%SRC_FILE%) do ffmpeg_hevc_qsv.bat "%%i"
+for /f "delims=" %%i in (%SRC_FILE%) do ffmpeg_hevc_nvenc_s.bat "%%i"
