2a682fedfe73f7cb87ced702ed5950a9cf0f4e0d
Andreas Zhang <denglitsch@gmail.com>
2026-03-15 09:44:34 +0800

    update: size and bit_rate提取优化; qsv和cuda硬件加速优化 for win

--- ffmpeg_hevc_qsv.bat
+++ ffmpeg_hevc_qsv.bat
@@ -19,7 +19,8 @@ set FFMPEG_PATH=C:\Program Files\ffmpeg\bin\ffmpeg.exe
 if not defined FFMPEG_PATH goto NO_PATH_ERR
 echo 已找到ffmpeg于:%FFMPEG_PATH%
 ::set "RUN_COM="%FFMPEG_PATH%" -hide_banner -threads 0 -hwaccel qsv -hwaccel_output_format qsv"
-set "RUN_COM="%FFMPEG_PATH%" -hide_banner -threads 0 -v verbose -hwaccel auto"
+::set "RUN_COM="%FFMPEG_PATH%" -hide_banner -threads 0 -v verbose -hwaccel auto"
+set "RUN_COM="%FFMPEG_PATH%" -hide_banner -threads 0 -v verbose -init_hw_device qsv=hw -filter_hw_device hw"
 
 SET "SRC_FILE="
 
@@ -45,7 +46,8 @@ set SRC_FILE="%SRC_FILE:"=%"
 echo SRC_FILE:%SRC_FILE%
 ::pause
 
-SET "RUN_COM=%RUN_COM% -i %SRC_FILE:&=^&%"
+::SET "RUN_COM=%RUN_COM% -i %SRC_FILE:&=^&%"
+SET "RUN_COM=%RUN_COM% -i %SRC_FILE:&=^&% -vf "format=nv12,hwupload=extra_hw_frames=64""
 echo cccccccc
 echo RUN_COM0=%RUN_COM%
 
@@ -189,6 +191,9 @@ set "SRC_SIZE="%FFPROBE_PATH%" -v error -hide_banner -show_entries format=size -
 %SRC_SIZE% > "size"
 set /p SRC_SIZE=<"size"
 del "size"
+if %SRC_SIZE% leq 0 (
+   for %%A in (%SRC_FILE%) do set SRC_SIZE=%%~zA
+)
 echo SRC_SIZE=%SRC_SIZE%
 
 set "SRC_DURATION="%FFPROBE_PATH%" -v error -hide_banner -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 %SRC_FILE:&=^&%"
@@ -199,11 +204,12 @@ echo SRC_DURATION=%SRC_DURATION%
 
 
 ::set "SRC_BITRATE="
-set "SRC_BITRATE="%FFPROBE_PATH%" -v error -hide_banner -of default=noprint_wrappers=0 -select_streams v:0 -show_entries stream=bit_rate -of csv=p=0:s=x %SRC_FILE:&=^&%"
+::set "SRC_BITRATE="%FFPROBE_PATH%" -v error -hide_banner -of default=noprint_wrappers=0 -select_streams v:0 -show_entries stream=bit_rate -of csv=p=0:s=x %SRC_FILE:&=^&%"
+set "SRC_BITRATE="%FFPROBE_PATH%" -v error -hide_banner -show_entries format=bit_rate -of default=noprint_wrappers=1:nokey=1 %SRC_FILE:&=^&%"
 ::for /f "delims=" %%i in ('"%SRC_BITRATE%"') do set SRC_BITRATE=%%i
-%SRC_BITRATE% > "temp"
-set /p SRC_BITRATE=<"temp"
-del "temp"
+%SRC_BITRATE% > "bit_rate"
+set /p SRC_BITRATE=<"bit_rate"
+del "bit_rate"
 ::echo SRC_BITRATE0=%SRC_BITRATE%
 set /a SRC_BITRATE=%SRC_BITRATE%
 IF not %ERRORLEVEL% NEQ 0 ( 
@@ -478,7 +484,8 @@ echo TARGET_BITRATE=%BIT%
 ::pause
 ::echo RUN_COM0:%RUN_COM%
 ::if defined BIT set "RUN_COM=%RUN_COM:&=^&% -c:v hevc_qsv -fps_mode cfr -profile:v main -preset veryfast -b:v %BIT% -pix_fmt nv12 -g 250 -keyint_min 25 -sws_flags bicubic -ar 44100 -b:a 128k -c:a aac -ac 2 -map_metadata -1 -map_chapters -1 -strict -2 -rtbufsize 120m -max_muxing_queue_size 1024"
-if defined BIT set "RUN_COM=%RUN_COM:&=^&% -c:v hevc_qsv -profile:v main -preset veryfast -b:v %BIT% -fps_mode cfr -pix_fmt yuv420p -color_range tv -colorspace bt709 -color_primaries bt709 -color_trc bt709 -g 250 -keyint_min 25 -sws_flags bicubic -ar 44100 -b:a 128k -c:a aac -ac 2 -map_metadata -1 -map_chapters -1 -strict -2 -rtbufsize 120m -max_muxing_queue_size 1024"
+::if defined BIT set "RUN_COM=%RUN_COM:&=^&% -c:v hevc_qsv -profile:v main -preset veryfast -b:v %BIT% -fps_mode cfr -pix_fmt yuv420p -color_range tv -colorspace bt709 -color_primaries bt709 -color_trc bt709 -g 250 -keyint_min 25 -sws_flags bicubic -ar 44100 -b:a 128k -c:a aac -ac 2 -map_metadata -1 -map_chapters -1 -strict -2 -rtbufsize 120m -max_muxing_queue_size 1024"
+if defined BIT set "RUN_COM=%RUN_COM:&=^&% -c:v hevc_qsv -profile:v main -preset veryfast -b:v %BIT% -fps_mode cfr -color_range tv -colorspace bt709 -color_primaries bt709 -color_trc bt709 -g 250 -keyint_min 25 -ar 44100 -b:a 128k -c:a aac -ac 2 -map_metadata -1 -map_chapters -1 -strict -2 -rtbufsize 120m -max_muxing_queue_size 1024"
 echo RUN_COM2:%RUN_COM%
 
 echo.
